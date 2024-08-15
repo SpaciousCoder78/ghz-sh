891cce7911ee4d87ac6e671ccfc4cfa8bf954115
SpaciousCoder78 <aryankmmiv@outlook.com>
2024-08-15 09:13:43 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//main file
+#define EXIT_SUCCESS 0
+
+int main(int argc, char **argv){
+
+    //area for config files
+
+    //call the loop
+    ghzsh_loop();
+
+    //space for cleanup
+
+    return EXIT_SUCCESS;
+
+}
